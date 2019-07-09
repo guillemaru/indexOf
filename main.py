3ab5881a08ee10d517eb065b84586ff7636f575d
guillemaru <35870448+guillemaru@users.noreply.github.com>
2019-07-09 22:40:32 +0200

    Update and rename Solution.py to main.py

--- main.py
+++ main.py
@@ -21,13 +21,25 @@ class Solution:
                     
                 
 def main():
-	haystack = "hello"
-	needle = "ll"
-	solution = Solution()
-	print("haystack is " + haystack + "; needle is " + needle + "solution is " + solution.strStr(haystack, needle))
-  	haystack = "aaalaaallaaaa"
-  	print("haystack is " + haystack + "; needle is " + needle + "solution is " + solution.strStr(haystack, needle))
-  	haystack = "aaaaaaaa"
-  	print("haystack is " + haystack + "; needle is " + needle + "solution is " + solution.strStr(haystack, needle))
+    haystack = "hello"
+    needle = "ll"
+    solution = Solution()
+    print("haystack is " + str(haystack) + "; needle is " + str(needle) + "; solution is " + str(solution.strStr(haystack, needle)))
+    haystack = "aaaasfafnjllaaa"
+    print("haystack is " + str(haystack) + "; needle is " + str(needle) + "; solution is " + str(solution.strStr(haystack, needle)))
+    haystack = "aaaaaaaaaaa"
+    print("haystack is " + str(haystack) + "; needle is " + str(needle) + "; solution is " + str(solution.strStr(haystack, needle)))
+    
+    
 if __name__ == '__main__':
     	main()
+	
+	
+'''
+Output is
+
+haystack is hello; needle is ll; solution is 2
+haystack is aaaasfafnjllaaa; needle is ll; solution is 10
+haystack is aaaaaaaaaaa; needle is ll; solution is -1
+
+'''
